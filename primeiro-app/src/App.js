a8fbc0ddb2c047e25495367a574a93f92b90de96
Tiago Sebastian <tiagosebastian99@gmail.com>
2022-06-24 05:19:12 -0300

    Propriedade e componente

---
+++ primeiro-app/src/App.js
@@ -0,0 +1,14 @@
+import Nome from './components/Nome';
+
+function App() {
+  return (
+    <div>
+      <h1>Componente App</h1>
+      <Nome aluno="Lucas" idade={30} />
+      <br/>
+      <Nome aluno="Tiago" idade={25} />
+    </div>
+  );
+}
+
+export default App;
