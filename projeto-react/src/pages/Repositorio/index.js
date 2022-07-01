bcab156383e4f71038d914c8b490a0746fbb2505
Tiago Sebastian <tiagosebastian99@gmail.com>
2022-07-01 00:55:03 -0300

    Exibindo issues

--- projeto-react/src/pages/Repositorio/index.js
+++ projeto-react/src/pages/Repositorio/index.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
-import {Container, Owner, Loading, BackButton} from './styles';
-import {FaArrowLeft} from 'react-icons/fa';
+import {Container, Owner, Loading, BackButton, IssuesList} from './styles';
+import { FaArrowLeft } from 'react-icons/fa';
 import api from '../../services/api';
 
 export default function Repositorio({match}){
@@ -26,6 +26,8 @@ export default function Repositorio({match}){
 
       setRepositorio(repositorioData.data);
       setIssues(issuesData.data);
+      console.log(issuesData.data);
+
       setLoading(false);
 
     }
@@ -34,26 +36,53 @@ export default function Repositorio({match}){
 
   }, [match.params.repositorio]);
 
+  
   if(loading){
     return(
       <Loading>
-        <h1>Carregando</h1>
+        <h1>Carregando...</h1>
       </Loading>
     )
   }
-
+  
   return(
     <Container>
-      <BackButton to="/">
-        <FaArrowLeft color='#000' size={30}/>
-      </BackButton>
-      <Owner>
-        <img src={repositorio.owner.avatar_url}
-        alt={repositorio.owner.login}
-        />
-        <h1>{repositorio.name}</h1>
-        <p>{repositorio.description}</p>
-      </Owner>
+        <BackButton to="/">
+          <FaArrowLeft color="#000" size={30} />
+        </BackButton>
+
+        <Owner>
+          <img 
+          src={repositorio.owner.avatar_url} 
+          alt={repositorio.owner.login} 
+          />
+          <h1>{repositorio.name}</h1>
+          <p>{repositorio.description}</p>
+        </Owner>
+
+        <IssuesList>
+          {issues.map(issue => (
+            <li key={String(issue.id)}>
+              <img src={issue.user.avatar_url} alt={issue.user.login} />
+
+              <div>
+                <strong>
+                  <a href={issue.html_url}>{issue.title}</a>
+
+                  {issue.labels.map(label => (
+                    <span key={String(label.id)}>{label.name}</span>
+                  ))}
+
+                </strong>
+
+                <p>{issue.user.login}</p>
+
+              </div>
+
+            </li>
+          ))}
+        </IssuesList>
+
     </Container>
   )
 }
