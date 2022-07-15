f38a3c50e9a8b9e3473213785f6e5635556508d1
Tiago Sebastian <tiagosebastian99@gmail.com>
2022-07-15 04:52:01 -0300

    Entendendo App

--- nextjs/projeto-board/src/pages/index.tsx
+++ nextjs/projeto-board/src/pages/index.tsx
@@ -1,10 +1,17 @@
 
+import Head from 'next/head';
 import styles from '../styles/style.module.scss';
 
 export default function Home() {
   return (
+    <>
+     <Head>
+        <title>Board - Organizando suas tarefas.</title>
+      </Head>
     <div>
+      
       <h1 className={styles.title}>Olá Mundo!</h1>
     </div>
+    </>
   )
 }
