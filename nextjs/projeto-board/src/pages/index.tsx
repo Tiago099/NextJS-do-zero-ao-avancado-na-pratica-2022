25ba557fddd8cc32fa9fbc1e53bed97f0df09e8f
Tiago Sebastian <tiagosebastian99@gmail.com>
2022-07-15 04:26:07 -0300

    Adicionando Sass

--- nextjs/projeto-board/src/pages/index.tsx
+++ nextjs/projeto-board/src/pages/index.tsx
@@ -1,9 +1,10 @@
 
+import styles from '../styles/style.module.scss';
 
 export default function Home() {
   return (
     <div>
-      Olá Mundo!
+      <h1 className={styles.title}>Olá Mundo!</h1>
     </div>
   )
 }
