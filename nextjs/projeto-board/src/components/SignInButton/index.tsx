4e00e7e35f964157df058a859ef6a0c163d5bc7a
Tiago Sebastian <tiagosebastian99@gmail.com>
2022-07-26 18:52:43 -0300

    Autenticação do usários

--- nextjs/projeto-board/src/components/SignInButton/index.tsx
+++ nextjs/projeto-board/src/components/SignInButton/index.tsx
@@ -1,32 +1,32 @@
 
+import { signIn, signOut, useSession} from 'next-auth/react';
+
 import styles from './styles.module.scss';
-import  { FaGithub } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi';
 
+
 export function SignInButton(){
+    const { data: session } = useSession();
 
-    const session = true;
-
-    return session ? (
-        <button 
-        type="button" 
-        className={styles.signInButton}
-        onClick={ () => {} }
-        >
-            <img src='https://sujeitoprogramador.com/steve.png' alt='Foto do usuario'/>
-            Olá Tiago
-            <FiX color='#737380' className={styles.closeIcons} />      
-             
-        </button>
-    ) : (
-        <button 
-        type="button" 
-        className={styles.signInButton}
-        onClick={ () => {} }
-        >
-            <FaGithub color='#FFB800' />      
-             Entra com o github
-        </button>
-    )
-      
+  return session ? (
+    <button
+    type="button"
+    className={styles.signInButton}
+    onClick={ () => signOut() }
+    >
+      <img src={session.user.image} alt="Foto do usuario" />
+      Olá {session.user.name}
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button
+    type="button"
+    className={styles.signInButton}
+    onClick={ () => signIn('github') }
+    >
+      <FaGithub color="#FFB800"/>
+      Entrar com github
+    </button>
+  )
 }
